ce92e78d3a8e24a8cadcb305fabaa24f37606d00
Jonathan <jonathan.corallo.utn@gmail.com>
2014-12-01 00:20:27 -0300

    add Moment.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+
+var moment = require('moment');
 var sys = require("sys"),  
 my_http = require("http");  
 my_http.createServer(function(request,response){  
     sys.puts("solicitud recibida");  
-    response.writeHeader(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World");  
-    response.end();  
+	response.writeHeader(200, {"Content-Type": "text/plain"});  
+	response.write("Hello World! La fecha y hora actual es: ");
+	response.write(moment().locale('es').format('LLLL'));  
+	response.end();  
 }).listen(8080);  
-sys.puts("Server Running on 8080"); 
+sys.puts("Server Running on 8080");
