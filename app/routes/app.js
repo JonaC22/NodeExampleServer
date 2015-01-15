ab4f035ee4ae46a26aa3fd8819f96411bbc1c017
Jonathan <jonathan.corallo.utn@gmail.com>
2015-01-15 09:05:23 -0300

    add mongo example query

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,4 +1,11 @@
 // app.js 
 var databaseUrl = "mydb"; 
 // "username:password@example.com/myd 
-var collections = ["users", "reports"] var db = require("mongojs").connect(databaseUrl, collections);
+
+//collections es un set (array) de colecciones
+var collections = ["users", "reports"] 
+var db = require("mongojs").connect(databaseUrl, collections); 
+
+//query de ejemplo, la funcion callback manipula los resultados cuando se obtienen
+//loguea por consola los usuarios mayores de edad
+db.users.find({age > 18}, function(err, users) { if( err || !users) console.log("No adult users found"); else users.forEach( function(adultUser) { console.log(adultUser); } ); });
